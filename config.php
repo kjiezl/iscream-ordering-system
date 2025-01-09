cd39d58ca32404277734c5503a08fd0b80c53202
Kwenn Jiezl <kjiezl08@gmail.com>
2025-01-09 18:08:26 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'shop_db', 3307);
+// if (!$conn) {
+//     die('Connection failed: ' . mysqli_connect_error());
+// }
+// echo 'Connected successfully';
+?>
